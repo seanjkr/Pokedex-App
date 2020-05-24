63ea0ea405b6092c9c0f287f505d2062e4c98f17
seanjkr <seankoppreddy@gmail.com>
2020-05-23 20:36:25 -0500

    Added Pokemon List, updated title of site

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world');
-
-var favoriteFood='Pizza';
-document.write(favoriteFood);
+var pokemonList= [
+  {name: 'Bulbasaur', height: 0.7, type:['Grass', 'Poison'], HP: 45},
+  {name: 'Ivysaur', height: 1, type:['Grass', 'Poison'],  HP: 60},
+  {name: 'Venusaur', height: 2, type:['Grass', 'Poison'], HP: 80},
+  {name:'Charmander', height: 0.6, type:'Fire', HP: 39},
+  {name:'Charmeleon', height: 1.1, type:'Fire', HP: 58},
+  {name:'Charizard', height: 1.7, type:'Fire', HP: 78},
+  {name: 'Squirtle', height: 0.5, type:'Water', HP: 44},
+  {name: 'Wartortle', height: 1, type:'Water', HP: 59},
+  {name: 'Blastoise', height: 1.6, type:'Water', HP: 79},
+];
